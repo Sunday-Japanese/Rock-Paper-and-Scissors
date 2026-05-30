0fec850a45213d2e1ff127419a8d095fad754f16
Yashas <yashasb2008@gmail.com>
2026-05-30 16:23:54 +0530

    basic UI completed

--- start/foundation.js
+++ start/foundation.js
@@ -21,80 +21,130 @@ function playRound(getHumanChoice,getComputerChoice) {
     let human = getHumanChoice;
     let gadget = getComputerChoice;
     if (human === "rock" && gadget === "paper")  {
-        return 'You Lose! Paper beats Rock'
         computerScore++;
+        return `You Lose! Paper beats Rock \n
+                Your Score: ${humanScore} | Computer Score ${computerScore}`       
     } else if (human === "paper" && gadget === "rock") {
-        return 'You Win! Paper beats Rock'
         humanScore++;
+        return `You Win! Paper beats Rock \n
+                Your Score: ${humanScore} | Computer Score ${computerScore}`        
         } else if (human === "scissors" && gadget === "rock") {
-        return "You Lose! Rock beats Scissors" 
             computerScore++;
+        return `You Lose! Rock beats Scissors \n 
+                Your Score: ${humanScore} | Computer Score ${computerScore}`            
             } else if (human ==="rock" && gadget === "scissors") {
-                return "You Win! Rock Beats Scissors"
                 humanScore++;
-            } else if (human ==="paper" && gadget === "scissors") {
-                return "You Lose! Scissors beats Paper" 
-                computerScore++;
-                } else if (human === "scissors" &&  gadget === "paper"){
-                    return "You Win! Scissors Beats Paper"
-                    humanScore++;
-                        } else if (human === "rock" && gadget === "rock" ) {
-                            return "It's draw ! Rock is same as Rock"
+                return `You Win! Rock Beats Scissors \n
+                        Your Score: ${humanScore} | Computer Score ${computerScore}`                
+                    } else if (human ==="paper" && gadget === "scissors") {
+                        computerScore++;
+                return `You Lose! Scissors beats Paper \n
+                Your Score: ${humanScore} | Computer Score ${computerScore}`
+                        } else if (human === "scissors" &&  gadget === "paper"){
+                        humanScore++;
+                    return `You Win! Scissors Beats Paper \n
+                            Your Score: ${humanScore} | Computer Score ${computerScore}`
+                                } else if (human === "rock" && gadget === "rock" ) {
+                            return `It's draw ! Rock is same as Rock
+                                    Your Score: ${humanScore} | Computer Score ${computerScore}`
                             } else if (human === "paper" && gadget === "paper") {
-                                return "It's draw Paper is same as paper"
+                                return `It's draw Paper is same as paper
+                                Your Score: ${humanScore} | Computer Score ${computerScore}`
                             } else if (human === "scissors" && gadget === "scissors") {
-                                return "It's a draw! Scissors is same as Scissors"
+                                return `It's a draw! Scissors is same as Scissors
+                                        Your Score: ${humanScore} | Computer Score ${computerScore}`
                                 } else {
                                     return "why? seriously are you dumb or just stupid!"
                             }
-    return `Your Score: ${humanScore} | Computer Score ${computerScore}`
+    
 }
+
+function playGame() {
+        switch (true) {
+            case (humanScore === 5):
+            
+            return `Lol You Somehow Won 
+            ${buttonOne.disabled = true}
+            ${buttonTwo.disabled = true}
+            ${buttonThree.disabled = true} `            
+            break;
+            case (computerScore === 5):
+            return `You Suck Computer Won! 
+            ${buttonOne.disabled = true}
+            ${buttonTwo.disabled = true}
+            ${buttonThree.disabled = true} `
+            break;
+            default:
+            return ""
+        }
+    }
+    
+
 
 
 let computerSelection = getComputerChoice;
 
+let resetButton = document.createElement("button");
 let buttonOne = document.createElement("button");
 let buttonTwo = document.createElement("button");
 let buttonThree = document.createElement("button");
 let divElement = document.createElement("div");
 
 buttonOne.addEventListener("click", () => {
-    
+    let newRuler = document.createElement("hr")
+        
     let divRock = document.createElement("p")
-    divRock.textContent = "you selected | ROCK"
+    divRock.textContent = `You selected | ROCK \n
+    Computer selected | ${getComputerChoice().toUpperCase()} \n
+    ${playRound("rock",getComputerChoice())} \n
+    ${playGame()}
+    `
     divElement.appendChild(divRock)
+    divElement.appendChild(newRuler);
+
+    
 });
 
 buttonTwo.addEventListener("click",() => {
-let divRock = document.createElement("p")
-divRock.textContent = "you selected | PAPER"
-divElement.appendChild(divRock)
-})
+let newRuler = document.createElement("hr")
+        
+    let divRock = document.createElement("p")
+    divRock.textContent = `You selected | PAPER \n
+    Computer selected | ${getComputerChoice().toUpperCase()} \n
+    ${playRound("paper",getComputerChoice())} \n
+    ${playGame()}
+    `
+    divElement.appendChild(divRock)
+    divElement.appendChild(newRuler);
+});
 
 buttonThree.addEventListener("click",() => {
-let divRock = document.createElement("p")
-divRock.textContent = "you selected | SCISSORS"
-divElement.appendChild(divRock)
+let newRuler = document.createElement("hr")
+        
+    let divRock = document.createElement("p")
+    divRock.textContent = `You selected | SCISSORS \n
+    Computer selected | ${getComputerChoice().toUpperCase()} \n
+    ${playRound("scissors",getComputerChoice())} \n
+    ${playGame()}
+    `
+    divElement.appendChild(divRock)
+    divElement.appendChild(newRuler);
 })
+
+resetButton.addEventListener("click",() => location.reload())
+
+
 
 buttonOne.innerText = "rock"
 buttonTwo.innerText = "paper"
 buttonThree.innerText = "scissors"
-
-let humanPara = document.createElement("p")
-let computerPara = document.createElement("p")
-let scoreHead = document.createElement("h1")
-let resultPara = document.createElement("p")
-
-
-//computerPara.innerText = `computer selected | ${getComputerChoice().toUpperCase()}`
-//divElement.appendChild(computerPara)
-
+resetButton.innerText = "Restart"
 
 
 document.body.appendChild(buttonOne)
 document.body.appendChild(buttonTwo)
 document.body.appendChild(buttonThree)
+document.body.appendChild(resetButton)
 document.body.appendChild(divElement)
 
 
